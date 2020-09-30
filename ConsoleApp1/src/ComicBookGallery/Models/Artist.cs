c3dc90b3b06dc1b7991a75c6d4ccc8dbe1ade152
Charles Hapner <chapner91@gmail.com>
2020-09-30 15:02:41 -0400

    Added comicbook and artist models

---
+++ ConsoleApp1/src/ComicBookGallery/Models/Artist.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+	public class Artist
+	{
+		public string Role { get; set; }
+		public string Name { get; set; }
+	}
+}
---
+++ ConsoleApp1/src/ComicBookGallery/Models/ComicBook.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ComicBookGallery.Models
+{
+	public class ComicBook
+	{
+		public int ID { get; set; }
+		public string SeriesTitle { get; set; }
+		public int IssueNumber { get; set; }
+		public string DescriptionHtml { get; set; }
+		public List<Artist> Artitsts { get; set; }
+		public bool Favorite { get; set; }
+		//{
+		//	"Script: Dan Slott",
+		//	"Pencils: Humberto Ramos",
+		//	"Inks: Victor Olazaba",
+		//	"Colors: Edgar Delgado",
+		//	"Letters: Chris Eliopoulos"
+		//};
+	}
+}
